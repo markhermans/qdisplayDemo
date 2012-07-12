6ad71490cf8913abbf5b3596c7ba5631ff14d297
Mark Hermansdorfer <mark.hermansdorfer@mnemonics-esd.com>
2012-07-12 17:50:38 -0400

    red ADI on sky/ground ADI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,21 +15,26 @@ MainWindow::MainWindow(QWidget *parent)
 
     threadA.someMethod();
 
-    QGraphicsPixmapItem *bg = new QGraphicsPixmapItem(QPixmap(":/images/plane.jpg"));
-    QGraphicsPixmapItem *fg = new QGraphicsPixmapItem(QPixmap(":/images/gimp.jpg"));
-    QGraphicsPixmapItem *fg2 = new QGraphicsPixmapItem(QPixmap(":/images/gimp.jpg"));
+    QGraphicsPixmapItem *bg = new QGraphicsPixmapItem(QPixmap(":/images/adi_600_600.jpg"));
+//    QGraphicsPixmapItem *bg0 = new QGraphicsPixmapItem(QPixmap(":/images/background_m1.jpg"));
+//    QGraphicsPixmapItem *bg1 = new QGraphicsPixmapItem(QPixmap(":/images/adi_land.jpg"));
+    QGraphicsPixmapItem *fg = new QGraphicsPixmapItem(QPixmap(":/images/adi_360_480.jpg"));
+//    QGraphicsPixmapItem *fg2 = new QGraphicsPixmapItem(QPixmap(":/images/gimp.jpg"));
 
     scene = new QGraphicsScene;
     scene->setItemIndexMethod(QGraphicsScene::NoIndex);
     scene->addItem(bg);
+//    scene->addItem(bg0);
+//    scene->addItem(bg1);
     scene->addItem(fg);
-    scene->addItem(fg2);
+//    scene->addItem(fg2);
 
     view = new QGraphicsView(scene);
     view->setRenderHint(QPainter::Antialiasing);
     view->setCacheMode(QGraphicsView::CacheBackground);
     view->setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
     view->setDragMode(QGraphicsView::ScrollHandDrag);
+//    view->setStyleSheet("background-color: transparent;");
 
     moveButton = new QPushButton(tr("&Move"));
     connect(moveButton, SIGNAL(clicked()), this, SLOT(move()));
@@ -50,9 +55,13 @@ MainWindow::MainWindow(QWidget *parent)
     vlayout->addLayout(hlayout);
     setLayout(vlayout);
 
-    timer = new QTimer();
-    connect(timer, SIGNAL(timeout()), this, SLOT(move()));
-    timer->start(1000/33);
+    fg->setPos(120,60);
+    fg->rotate(53.13);
+    fg->setPos(384,12);
+
+//    timer = new QTimer();
+//    connect(timer, SIGNAL(timeout()), this, SLOT(move()));
+//    timer->start(1000/33);
 
     setWindowTitle(tr("A Dialog"));
 }
@@ -76,15 +85,15 @@ void MainWindow::move()
 {
     QList<QGraphicsItem *> syms = scene->items();
 
-    QPointF pos1 = syms[1]->scenePos();
+    QPointF pos1 = syms[2]->scenePos();
     float dx = float(qrand() % 11)/10.0;
     float dy = float(qrand() % 18)/10.0;
-    syms[1]->setPos(pos1.rx()+dx,pos1.ry()+dy);
+    syms[2]->setPos(pos1.rx()+dx,pos1.ry()+dy);
 
-    QPointF pos2 = syms[2]->scenePos();
+    QPointF pos2 = syms[3]->scenePos();
     float dx2 = float(qrand() % 12)/10.0;
     float dy2 = float(qrand() % 9)/10.0;
-    syms[2]->setPos(pos2.rx()+dx2,pos2.ry()+dy2);
+    syms[3]->setPos(pos2.rx()+dx2,pos2.ry()+dy2);
 }
 
 void MainWindow::startOrStopThread()
