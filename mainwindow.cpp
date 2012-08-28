664643d97060b1018961037b200d30055984b68a
Mark Hermansdorfer <mark.hermansodrfer@mnemonics-esd.com>
2012-08-28 15:45:38 -0400

    removed 'close' button, adjusted window geometyr

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -139,6 +139,8 @@ MainWindow::MainWindow(QWidget *parent)
     view->setCacheMode(QGraphicsView::CacheBackground);
     view->setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
     view->setDragMode(QGraphicsView::ScrollHandDrag);
+    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     moveButton = new QPushButton(tr("&Move"));
     connect(moveButton, SIGNAL(clicked()), this, SLOT(move()));
@@ -146,24 +148,15 @@ MainWindow::MainWindow(QWidget *parent)
     threadButton = new QPushButton(tr("&Start Thread"));
     connect(threadButton, SIGNAL(clicked()), this, SLOT(startOrStopThread()));
 
-    closeButton = new QPushButton(tr("&Close"));
-    connect(closeButton, SIGNAL(clicked()), this, SLOT(close()));
-
-    QHBoxLayout *hlayout = new QHBoxLayout;
-//    hlayout->addWidget(moveButton);
-//    hlayout->addWidget(threadButton);
-    hlayout->addWidget(closeButton);
-
     QVBoxLayout *vlayout = new QVBoxLayout;
     vlayout->addWidget(view);
-    vlayout->addLayout(hlayout);
     setLayout(vlayout);
 
     fg->setPos(100,125);
 
     // top labels
     baseX = 75;
-    baseY = 0;
+    baseY = 25;
     offsetX = 88;
     t1_T->setPos(baseX, baseY);
     t2_T->setPos(baseX+offsetX, baseY);
@@ -173,8 +166,8 @@ MainWindow::MainWindow(QWidget *parent)
 
     // left side labels
     baseX = 0;
-    baseY = 78; //100;
-    offsetY = 60; //61;
+    baseY = 112; //100;
+    offsetY = 52; //61;
     t1_L->setPos(baseX, baseY);
     t2_L->setPos(baseX, baseY + offsetY);
     t3_L->setPos(baseX, baseY + 2*offsetY);
@@ -188,7 +181,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     // bottom labels
     baseX = 75;
-    baseY = 685;
+    baseY = 660; //685;
     offsetX = 88;
     t1_B->setPos(baseX, baseY);
     t2_B->setPos(baseX+offsetX, baseY);
@@ -198,8 +191,8 @@ MainWindow::MainWindow(QWidget *parent)
 
     // right side labels
     baseX = 514;
-    baseY = 78;
-    offsetY = 60;
+    baseY = 112;
+    offsetY = 52;
     t1_R->setPos(baseX, baseY);
     t2_R->setPos(baseX, baseY + offsetY);
     t3_R->setPos(baseX, baseY + 2*offsetY);
@@ -223,6 +216,12 @@ MainWindow::MainWindow(QWidget *parent)
 //    timer->start(100);
 
     setWindowTitle(tr("A Dialog"));
+    QRect myGeometry = this->geometry();
+    this->setGeometry(
+	myGeometry.x()+325,
+	myGeometry.y()+30,
+	myGeometry.width(),
+	myGeometry.height()+200);
 }
 
 void MainWindow::closeEvent(QCloseEvent *event)
